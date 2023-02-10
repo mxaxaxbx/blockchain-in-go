aa2f8b4b8fa6bfa319a74c2f3a00aacc41c844fa
Miguel A Arenas <miguel.arenas@sitidoctor.com>
2023-02-10 15:30:55 -0500

    main blockchain methods has been created

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ type Block struct {
 	Hash          []byte
 }
 
+type Blockchain struct {
+	blocks []*Block
+}
+
 func (b *Block) SetHash() {
 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
 	headers := bytes.Join([][]byte{
@@ -35,4 +39,20 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 	}
 	block.SetHash()
 	return block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis block", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{
+		[]*Block{NewGenesisBlock()}
+	}
 }
